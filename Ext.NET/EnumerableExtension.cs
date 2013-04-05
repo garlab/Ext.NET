593744ed21348b766ec240137adfa965690b31be
Anthony Garcia <anthony.garcia-labiad@live.fr>
2013-04-05 11:25:13 +0200

    Add grep & egrep

--- Ext.NET/EnumerableExtension.cs
+++ Ext.NET/EnumerableExtension.cs
@@ -2,16 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Ext.NET
 {
     public static class EnumerableExtension
     {
-        public static void Each<T>(this IEnumerable<T> enumberable, Action<T> action)
+        public static void Each<T>(this IEnumerable<T> enumerable, Action<T> action)
         {
-            foreach (var item in enumberable)
+            foreach (var item in enumerable)
             {
                 action(item);
+            }
+        }
+
+        public static IEnumerable<T> Grep<T>(this IEnumerable<T> enumerable, string pattern)
+        {
+            foreach (var item in enumerable)
+            {
+                if (item.ToString().Contains(pattern))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<T> EGrep<T>(this IEnumerable<T> enumerable, string pattern, RegexOptions option = RegexOptions.None)
+        {
+            var regex = new Regex(pattern, option);
+            foreach (var item in enumerable)
+            {
+                if (regex.IsMatch(item.ToString()))
+                {
+                    yield return item;
+                }
             }
         }
     }
