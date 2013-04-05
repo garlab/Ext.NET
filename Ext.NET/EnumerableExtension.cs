52939a88a79d0970928842029d285c1b3afd0c3b
Anthony Garcia <anthony.garcia-labiad@live.fr>
2013-04-05 12:45:39 +0200

    Add doc

--- Ext.NET/EnumerableExtension.cs
+++ Ext.NET/EnumerableExtension.cs
@@ -4,19 +4,41 @@ using System.Text.RegularExpressions;
 
 namespace Ext.NET
 {
+    /// <summary>
+    /// Contain every extensions for Enumerable.
+    /// </summary>
     public static class EnumerableExtension
     {
-        public static void Each<T>(this IEnumerable<T> enumerable, Action<T> action)
+        /// <summary>
+        /// Inline foreach statement.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="action">Action applied on each element from the source</param>
+        public static void Each<T>(this IEnumerable<T> source, Action<T> action)
         {
-            foreach (var item in enumerable)
+            if (source == null) throw new ArgumentNullException("source");
+            if (action == null) throw new ArgumentNullException("action");
+
+            foreach (var item in source)
             {
                 action(item);
             }
         }
 
-        public static IEnumerable<T> Grep<T>(this IEnumerable<T> enumerable, string pattern)
+        /// <summary>
+        /// Keep only element containing the given pattern. Similar to unix grep.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="pattern">A simple string</param>
+        /// <returns></returns>
+        public static IEnumerable<T> Grep<T>(this IEnumerable<T> source, string pattern)
         {
-            foreach (var item in enumerable)
+            if (source == null) throw new ArgumentNullException("source");
+            if (pattern == null) throw new ArgumentNullException("pattern");
+
+            foreach (var item in source)
             {
                 if (item.ToString().Contains(pattern))
                 {
@@ -25,10 +47,22 @@ namespace Ext.NET
             }
         }
 
-        public static IEnumerable<T> EGrep<T>(this IEnumerable<T> enumerable, string pattern, RegexOptions option = RegexOptions.None)
+        /// <summary>
+        /// Keep only element matching the given pattern. Similar to unix egrep.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="pattern">A regex string</param>
+        /// <param name="option">Regex options, default None</param>
+        /// <returns></returns>
+        public static IEnumerable<T> EGrep<T>(this IEnumerable<T> source, string pattern, RegexOptions option = RegexOptions.None)
         {
+            if (source == null) throw new ArgumentNullException("source");
+            if (pattern == null) throw new ArgumentNullException("pattern");
+            if (option == null) throw new ArgumentNullException("option");
+
             var regex = new Regex(pattern, option);
-            foreach (var item in enumerable)
+            foreach (var item in source)
             {
                 if (regex.IsMatch(item.ToString()))
                 {
@@ -37,14 +71,32 @@ namespace Ext.NET
             }
         }
 
+        /// <summary>
+        /// Randomly permutes the source using the Fisher–Yates algorithm and a default source of randomness.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
         {
+            if (source == null) throw new ArgumentNullException("source");
+
             var random = new Random();
             return source.ShuffleIterator(random);
         }
 
+        /// <summary>
+        /// Randomly permute the source using the Fisher–Yates algorithm and the specified source of randomness.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="random">A random source</param>
+        /// <returns></returns>
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random random)
         {
+            if (source == null) throw new ArgumentNullException("source");
+            if (random == null) throw new ArgumentNullException("random");
+
             return source.ShuffleIterator(random);
         }
 
--- Ext.NET/IntegerExtension.cs
+++ Ext.NET/IntegerExtension.cs
@@ -5,16 +5,32 @@ using System.Text;
 
 namespace Ext.NET
 {
+    /// <summary>
+    /// Contain every extensions for Integer.
+    /// </summary>
     public static class IntegerExtension
     {
+        /// <summary>
+        /// Repeat an action n times.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="action">Action applied on each element from the source</param>
         public static void Times(this int n, Action<int> action)
         {
+            if (action == null) throw new ArgumentNullException("action");
+
             for (int i = 0; i < n; ++i)
             {
                 action(i);
             }
         }
 
+        /// <summary>
+        /// Generate a range from "n" to "to".
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="to"></param>
+        /// <returns></returns>
         public static IEnumerable<int> To(this int n, int to)
         {
             if (n == to)
