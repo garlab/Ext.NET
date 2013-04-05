1e1fde4599ed9ced41f0c0371e91553f07713bb6
Anthony Garcia <anthony.garcia-labiad@live.fr>
2013-04-05 11:00:38 +0200

    Add IEnumerable.Each(action) method

--- Ext.NET/EnumerableExtension.cs
+++ Ext.NET/EnumerableExtension.cs
@@ -7,5 +7,12 @@ namespace Ext.NET
 {
     public static class EnumerableExtension
     {
+        public static void Each<T>(this IEnumerable<T> enumberable, Action<T> action)
+        {
+            foreach (var item in enumberable)
+            {
+                action(item);
+            }
+        }
     }
 }
