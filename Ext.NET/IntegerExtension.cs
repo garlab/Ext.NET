00955c0ac2e970fc8697e15c365e16473eb8472e
Anthony Garcia <anthony.garcia-labiad@live.fr>
2013-04-05 10:48:14 +0200

    Add IntegerExtension class

---
+++ Ext.NET/IntegerExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ext.NET
+{
+    class IntegerExtension
+    {
+        public static void Times(this int n, Action<int> action)
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                action(i);
+            }
+        }
+    }
+}
